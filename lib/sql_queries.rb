48fd34e5a499e4f9f78c23d7b2f3a3bf523d2d88
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-28 20:14:32 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -12,7 +12,7 @@ end
 
 def selects_oldest_bear_and_returns_name_and_age
   # "SELECT name, age FROM bears WHERE MAX(age); "
-  "SELECT name, age FROM bears WHERE MAX(age);"
+  "SELECT name, age FROM bears WHERE age = MAX;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
