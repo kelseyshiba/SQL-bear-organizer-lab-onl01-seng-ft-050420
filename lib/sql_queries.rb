4a02e391a1792c70a29fbb0d4f73d9a461071b14
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-28 20:29:22 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -2,11 +2,11 @@ def selects_all_female_bears_return_name_and_age
   "SELECT name, age FROM bears WHERE gender IS 'Female';"
 end
 
-def selects_all_bears_names_and_orders_in_alphabetical_order
+def selects_all_bears_names_and_orders_in_alphabetical_order #DONE
   "SELECT name FROM bears ORDER BY name;"
 end
 
-def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
+def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest #DONE
   "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age;"
 end
 
