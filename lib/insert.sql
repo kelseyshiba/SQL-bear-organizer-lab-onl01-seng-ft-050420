1b8a41fc1327c331d0c5ad4924047385ba2561ac
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-28 20:18:15 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 34, "Male", "Black", "happy", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "Male", "Black", "crazy", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "Female", "White", "curious", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "Female", "White", "goofy", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 52, "Male", "Brown", "strict", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "Female", "Gold", "playful", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 65, "Male", "Green", "grouchy", 1);
